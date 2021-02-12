09c9b318982cb23e1b5cf53ebcb26d093a24b509
eni-kliszki <kliszkieniko@gmail.com>
2021-02-12 17:22:23 +0100

    add test

--- backend/src/main/java/com/codecool/backend/repository/UserRepository.java
+++ backend/src/main/java/com/codecool/backend/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.codecool.backend.repository;
 
 import com.codecool.backend.entity.ApplicationUser;
+import com.codecool.backend.modal.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository  extends JpaRepository<ApplicationUser, Long> {
+    List<ApplicationUser> findAllByLocation(Location location);
 }
